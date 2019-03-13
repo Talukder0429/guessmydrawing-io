202e5c5b0613daca308ef4d4ce67ddea2229a35a
RiazCharania <riaz123456789@hotmail.com>
2019-03-12 22:42:59 -0400

    added sizes, undo

--- static/js/api.js
+++ static/js/api.js
@@ -5,8 +5,10 @@
     let clickY = [];
     let clickDrag = [];
     let clickColor = [];
+    let clickSize = [];
     let paint = false;
     let currentColor = "#000000";
+    let currentSize = 5;
 
     let prepareCanvas = function(){
         canvas.onmousedown = function(e){
@@ -43,6 +45,7 @@
         clickY.push(y);
         clickDrag.push(dragging);
         clickColor.push(currentColor);
+        clickSize.push(currentSize);
     }
     
     let clearCanvas = function(){
@@ -50,37 +53,63 @@
     };
   
     let redraw = function(){
-      clearCanvas();
-      context.lineJoin = "round";
-      context.lineWidth = 5;
-      for(let i=0; i < clickX.length; i++)
-          {
-            if(clickDrag[i]){
-                      context.beginPath();
-                      context.moveTo(clickX[i-1], clickY[i-1]);
-                      context.lineTo(clickX[i], clickY[i]);
-                      context.closePath();
-                      context.strokeStyle = clickColor[i];
-                      context.stroke();
+        clearCanvas();
+        context.lineJoin = "round";
+        for (let i = 0; i < clickX.length; i++) {
+            if (clickDrag[i]) {
+                context.beginPath();
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+                context.lineTo(clickX[i], clickY[i]);
+                context.closePath();
+                context.strokeStyle = clickColor[i];
+                context.lineWidth = clickSize[i];
+                context.stroke();
             }
-          }
+        }
     };
 
     let resetCanvas = function(){
         clickX = [];
         clickY = [];
         clickDrag = [];
+        clickColor = [];
+        clickSize = [];
         clearCanvas();
     };
+
+    let undoLast = function(){
+        clickX.pop();
+        clickY.pop();
+        clickDrag.pop();
+        clickColor.pop();
+        clickSize.pop();
+        redraw();
+    }
 
     let changeColor = function(){
         currentColor = this.value;
     }
 
+    let setSizeSmall = function(){
+        currentSize = 2;
+    }
+
+    let setSizeRegular = function(){
+        currentSize = 5;
+    }
+
+    let setSizeBig = function(){
+        currentSize = 10;
+    }
+
     window.addEventListener('load', function(){
         prepareCanvas();
-        document.querySelector("#drawing > #erase").onclick = resetCanvas;
-        document.querySelector("#drawing > #canvasColor").onchange = changeColor;
+        document.querySelector("#drawing > #clear").onclick = resetCanvas;
+        document.querySelector("#drawing > #undo").onclick = undoLast;
+        document.querySelector("#drawing > #canvasColor").oninput = changeColor;
+        document.querySelector("#drawing > #size > #small").onclick = setSizeSmall;
+        document.querySelector("#drawing > #size > #regular").onclick = setSizeRegular;
+        document.querySelector("#drawing > #size > #big").onclick = setSizeBig;
     });
 
 })();
